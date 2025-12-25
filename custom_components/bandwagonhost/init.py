83e58b9309884d57cb8f1841ea18be853c524d08
nichwang88 <nic.hwang@hotmail.com>
2025-12-25 13:24:53 +0800

    Delete custom_components/bandwagonhost/init.py

--- custom_components/bandwagonhost/init.py
+++
@@ -1,6 +0,0 @@
-from homeassistant.core import HomeAssistant
-from .const import DOMAIN
-
-async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    hass.data.setdefault(DOMAIN, {})
-    return True
