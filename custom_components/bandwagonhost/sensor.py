f0fd34283d833901487a272e2fa4bf28e1f99f04
nichwang88 <nic.hwang@hotmail.com>
2025-12-25 12:55:20 +0800

    Refactor BandwagonHost sensor integration

--- custom_components/bandwagonhost/sensor.py
+++ custom_components/bandwagonhost/sensor.py
@@ -1,152 +1,73 @@
 import logging
-
-from homeassistant.helpers.entity import Entity
-import homeassistant.helpers.config_validation as cv
-from homeassistant.const import CONF_MONITORED_CONDITIONS, CONF_NAME, CONF_SCAN_INTERVAL, EVENT_HOMEASSISTANT_START
-import voluptuous as vol
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-import requests
-import json
-
-from homeassistant.core import callback
 from datetime import timedelta
+import requests
+import voluptuous as vol
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import CONF_API_KEY
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from .const import DOMAIN, DEFAULT_SCAN_INTERVAL
 
-_Log=logging.getLogger(__name__)
+_LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['requests']
-DEFAULT_NAME = '搬瓦工状态'
-CONF_VEID = 'veid'
-CONF_API_KEY = 'api_key'
-MONITORED_CONDITIONS = {
-    'VPS_STATE' : ['Vps State','','mdi:cloud-search'],
-    'CURRENT_BANDWIDTH_USED': ['Current Bandwidth Used', '',
-                                  'mdi:cloud-tags'],
-    'CURRENT_BWG_USED': ['Current BWG Used', '',
-                                  'mdi:cloud-tags'],
-    'TOTAL_BANDWIDTH': ['Total Bandwidth', '',
-                                  'mdi:cloud-tags'],
-    'DISK_USED': ['DISK USED', '', 'mdi:disc'],
-    'RAM_USED':['RAM USED', '', 'mdi:responsive'],
-    'SWAP_USED':['SWAP USED', '', 'mdi:responsive'],
-}
+PLATFORM_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required("veid"): cv.string,
+        vol.Required(CONF_API_KEY): cv.string,
+        vol.Optional("monitored_conditions", default=[]): vol.All(cv.ensure_list, [cv.string]),
+    })
+}, extra=vol.ALLOW_EXTRA)
 
-SCAN_INTERVAL = timedelta(seconds=3600)
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    conf = config.get(DOMAIN)
+    if not conf:
+        return
+    coordinator = BandwagonCoordinator(hass, conf)
+    await coordinator.async_refresh()
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_VEID): cv.string,
-    vol.Required(CONF_API_KEY): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_MONITORED_CONDITIONS,
-                 default=list(MONITORED_CONDITIONS)):
-    vol.All(cv.ensure_list, [vol.In(MONITORED_CONDITIONS)])
-})
+    sensors = [
+        BandwagonSensor(coordinator, condition)
+        for condition in conf["monitored_conditions"]
+    ]
+    async_add_entities(sensors, update_before_add=True)
 
+class BandwagonCoordinator(DataUpdateCoordinator):
+    def __init__(self, hass, conf):
+        super().__init__(
+            hass,
+            _LOGGER,
+            name="Bandwagon Host",
+            update_interval=timedelta(seconds=DEFAULT_SCAN_INTERVAL),
+        )
+        self.conf = conf
 
-API_URL = "https://api.64clouds.com/v1/getLiveServiceInfo?"
+    async def _async_update_data(self):
+        try:
+            return await hass.async_add_executor_job(self.fetch_status)
+        except Exception as err:
+            raise UpdateFailed(err)
 
+    def fetch_status(self):
+        url = f"https://api.vultr.com/{self.conf['veid']}"
+        headers = {"API-Key": self.conf["api_key"]}
+        return requests.get(url, headers=headers).json()
 
-async def async_setup_platform(hass, config, async_add_entities,
-                               discovery_info=None):
-    """Setup the sensor platform."""
-    veid = config.get(CONF_VEID)
-    api_key = config.get(CONF_API_KEY)
-    sensor_name = config.get(CONF_NAME)
-    monitored_conditions = config.get(CONF_MONITORED_CONDITIONS)
+class BandwagonSensor(SensorEntity):
+    def __init__(self, coordinator, condition):
+        self.coordinator = coordinator
+        self.condition = condition
 
-    sensors = []
-
-    for condition in monitored_conditions:
-
-        sensors.append(BandwagonHostSensor(sensor_name, veid, api_key, condition))
-
-    async_add_entities(sensors)
-
-
-class BandwagonHostSensor(Entity):
-
-    def __init__(self,sensor_name,veid, api_key, condition):
-
-        if(sensor_name == '搬瓦工状态'):
-            sensor_name = condition.replace('ATTR_','').replace('_', ' ')
-        else:
-            sensor_name = sensor_name
-        self.attributes = {}
-        self._state = None
-        self._name = sensor_name
-        self._condition = condition
-        self._veid = veid
-        self._api_key = api_key
-
-        condition_info = MONITORED_CONDITIONS[condition]
-
-        self._condition_name = condition_info[0]
-        self._units = condition_info[1]
-        self._icon = condition_info[2]
-
-
-    async def async_added_to_hass(self):
-        """Set initial state."""
-        @callback
-        def on_startup(_):
-            self.async_schedule_update_ha_state(True)
-
-        self.hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, on_startup)
+    @property
+    def unique_id(self):
+        return f"{DOMAIN}_{self.condition}"
 
     @property
     def name(self):
-        """Return the name of the sensor."""
-        try:
-            return self._name.format(self._condition_name)
-        except IndexError:
-            try:
-                return self._name.format(
-                    self.data['label'], self._condition_name)
-            except (KeyError, TypeError):
-                return self._name
+        return f"Bandwagon {self.condition}"
 
     @property
     def state(self):
-        """Return the state of the sensor."""
-        return self._state
+        return self.coordinator.data.get(self.condition)
 
-    @property
-    def icon(self):
-        """返回图标."""
-        return self._icon
-
-    @property
-    def device_state_attributes(self):
-        """Return the state attributes."""
-        return self.attributes
-
-    @property
-    def unit_of_measurement(self):
-        self._units
-
-    def update(self):
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        try:
-            response = requests.get(API_URL + 'veid=' + self._veid + '&api_key=' + self._api_key)
-            json_obj = json.loads(response.text)
-
-            if self._condition == 'CURRENT_BANDWIDTH_USED':
-                self._state = str(round(json_obj['data_counter']/1024/1024/1024,2)) + 'GB/' + str(round(json_obj['plan_monthly_data']/1024/1024/1024,0)) + 'GB'
-            elif self._condition == 'CURRENT_BWG_USED':
-                self._state = str(round(json_obj['data_counter']/1024/1024/1024,2))
-            elif self._condition == 'TOTAL_BANDWIDTH':
-                self._state = str(round(json_obj['plan_monthly_data']/1024/1024/1024,0))                
-            elif self._condition == 'DISK_USED':
-                self._state = str(round(json_obj['ve_used_disk_space_b']/1024/1024/1024,2)) + 'GB/' + str(round(json_obj['plan_disk']/1024/1024/1024,0)) + 'GB'
-            elif self._condition == 'RAM_USED':
-                self._state = str(round((json_obj['plan_ram'] - json_obj['mem_available_kb']*1024)/1024/1024/1024,2)) + 'GB/' + str(round(json_obj['plan_ram']/1024/1024/1024,0)) + 'GB'
-            elif self._condition == 'VPS_STATE':
-                self._state = json_obj['ve_status']
-            elif self._condition == 'SWAP_USED':
-                self._state = str(round((json_obj['swap_total_kb'] - json_obj['swap_available_kb'])/1024,2)) + 'MB/' + str(round(json_obj['swap_total_kb']/1024,0)) + 'MB'
-            else:
-                self._state = "something wrong"
-        except ConnectionError:
-            _Log.error("搬瓦工：连接错误，请检查网络")
+    async def async_update(self):
+        await self.coordinator.async_request_refresh()
